c3b51e8aaf176d490cf8dd3e7c4dc749055c4460
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-03-19 12:18:59 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-while true; do sleep 15 ; echo "background"; done &
+#!/bin/bash
+#while true; do sleep 15 ; echo "background"; done &
 
-while true; do sleep 12 ; echo "foreground"; done
+#while true; do sleep 12 ; echo "foreground"; done
+
+ oneShot() {
+    curl -s -k https://aa-paasport.paaseng.gslb.delta.com | grep LAB
+ }
+
+while true
+do
+
+ parallel -j10 oneShot 
+ sleep 1 
+
+done
+
